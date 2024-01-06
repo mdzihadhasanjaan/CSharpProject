b729906786f292f9b78d0c88c3605f875833a3f7
mdzihadhasanjaan <mdzihadhasanjaan@gmail.com>
2024-01-06 03:33:16 +0200

    code added to Inventory Calculation

--- InventoryCalculation/Program.cs
+++ InventoryCalculation/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿int[] inventory = { 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000 };
+int total = 0;
+int bin = 0;
+foreach(int items in inventory)
+{
+    total += items;
+    bin++;
+    Console.WriteLine($"Bin {bin} : {items} items (total running={total})");
+}
